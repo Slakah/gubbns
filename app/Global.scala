debfdda8ca59c3b71563e4204154ddc2a1c1d673
James Ross Collier <jrc.collier@gmail.com>
2013-10-28 23:49:00 +0000

    Create Global object

--- app/Global.scala
+++ app/Global.scala
@@ -1,5 +1,14 @@
-import play.api.GlobalSettings
+import play.api.{Application, GlobalSettings}
+import play.Logger
 
 object Global extends GlobalSettings {
 
+  override def onStart(app: Application) {
+    Logger.info("Application has started")
+    Logger.info("Yo dowg")
+  }
+
+  override def onStop(app: Application) {
+    Logger.info("Application shutdown...")
+  }
 }
