e3c4810ee617c4754333671357e61440d693eacf
James Ross Collier <jrc.collier@gmail.com>
2013-10-29 00:01:35 +0000

    Add partial couchdb config

--- app/Global.scala
+++ app/Global.scala
@@ -1,3 +1,4 @@
+import org.ektorp.http.{HttpClient, StdHttpClient}
 import play.api.{Application, GlobalSettings}
 import play.Logger
 
@@ -6,6 +7,13 @@ object Global extends GlobalSettings {
   override def onStart(app: Application) {
     Logger.info("Application has started")
     Logger.info("Yo dowg")
+
+
+    val authenticatedHttpClient: HttpClient  = new StdHttpClient.Builder()
+      .url("http://localhost:5984")
+      .username("admin")
+      .password("secret")
+      .build()
   }
 
   override def onStop(app: Application) {
