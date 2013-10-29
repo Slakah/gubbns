99d58f5185f418bb4ec7b6b1e2d75e1c7f8344f5
James Ross Collier <jrc.collier@gmail.com>
2013-10-29 23:46:30 +0000

    added basic couchdb config

--- app/Global.scala
+++ app/Global.scala
@@ -1,4 +1,6 @@
+import org.ektorp.{CouchDbConnector, CouchDbInstance}
 import org.ektorp.http.{HttpClient, StdHttpClient}
+import org.ektorp.impl.StdCouchDbInstance
 import play.api.{Application, GlobalSettings}
 import play.Logger
 
@@ -6,14 +8,18 @@ object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
     Logger.info("Application has started")
-    Logger.info("Yo dowg")
 
-
-    val authenticatedHttpClient: HttpClient  = new StdHttpClient.Builder()
+    val httpClient: HttpClient  = new StdHttpClient.Builder()
       .url("http://localhost:5984")
-      .username("admin")
-      .password("secret")
       .build()
+    Logger.info("Hmmpppf")
+    val dbInstance: CouchDbInstance = new StdCouchDbInstance(httpClient)
+    Logger.info("Hmmpppf2")
+
+    val db: CouchDbConnector = dbInstance.createConnector("blog", false)
+
+    Logger.info("Hmmpppf3")
+
   }
 
   override def onStop(app: Application) {
--- build.sbt
+++ build.sbt
@@ -6,7 +6,7 @@ version := "1.0-SNAPSHOT"
 
 libraryDependencies ++= Seq(
   "org.webjars" %% "webjars-play" % "2.2.0", 
-  "org.webjars" % "bootstrap" % "2.3.1",
+  "org.webjars" % "bootstrap" % "3.0.0",
   "org.ektorp" % "org.ektorp" % "1.2.0"
 )
 
