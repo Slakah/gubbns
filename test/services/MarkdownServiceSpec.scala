6fd26f66695ba22ba68d2ff88a7576bb9c1669db
James Collier <jrc.collier@gmail.com>
2014-06-05 10:21:16 -0400

    Add a retry to markdown spec, will prevent timeout due to warming up

--- test/services/MarkdownServiceSpec.scala
+++ test/services/MarkdownServiceSpec.scala
@@ -4,29 +4,33 @@ import org.specs2.mutable.Specification
 import scala.concurrent.ExecutionContext
 import ExecutionContext.Implicits.global
 import components.Pegdown
-
+import concurrent.duration._
 
 class MarkdownServiceSpec extends Specification with Pegdown {
   "MarkdownService" should {
 
     "transform # -> h1" in {
-      markdown("#test header").map(_.body) must equalTo("<h1>test header</h1>").await
+      markdown("#test header").map(_.body) must equalTo("<h1>test header</h1>").await(retries = 1)
     }
 
     "transform ## -> h2" in {
-      markdown("##test header").map(_.body) must equalTo("<h2>test header</h2>").await
+      markdown("##test header").map(_.body) must equalTo("<h2>test header</h2>").await(retries = 1)
+
     }
 
     "transform ### -> h3" in {
-      markdown("###test header").map(_.body) must equalTo("<h3>test header</h3>").await
+      markdown("###test header").map(_.body) must equalTo("<h3>test header</h3>").await(retries = 1)
+
     }
 
     "transform #### -> h4" in {
-      markdown("####test header").map(_.body) must equalTo("<h4>test header</h4>").await
+      markdown("####test header").map(_.body) must equalTo("<h4>test header</h4>").await(retries = 1)
+
     }
 
     "handle links" in {
-      markdown("[here](gubbns.com)").map(_.body) must contain("""<a href="gubbns.com">here</a>""").await
+      markdown("[here](gubbns.com)").map(_.body) must contain("""<a href="gubbns.com">here</a>""").await(retries = 1)
+
     }
   }
 }
