c9acecd8410cf03759d07c97f46b7dd89a832c28
James Collier <jrc.collier@gmail.com>
2014-07-02 22:05:12 +0100

    Add simple test to check PostsService uses PostRepositoryService

---
+++ test/services/PostServiceSpec.scala
@@ -0,0 +1,14 @@
+package services
+
+import org.specs2.mutable.Specification
+import repositories.{PostRepository, PostRepositoryComponent}
+
+class PostServiceSpec extends Specification with PostServiceComponent with PostRepositoryComponent {
+
+  "PostService" should {
+    "find post with title 'First!'" in {
+
+    }
+
+  }
+≈}
