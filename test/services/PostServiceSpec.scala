356db54077bad75c63c05087d341cf4a416ba769
James Collier <jrc.collier@gmail.com>
2014-07-02 22:29:07 +0100

    Add meat to PostServiceSpec

--- test/services/PostServiceSpec.scala
+++ test/services/PostServiceSpec.scala
@@ -1,14 +1,30 @@
 package services
 
+import models.Post
 import org.specs2.mutable.Specification
 import repositories.{PostRepository, PostRepositoryComponent}
+import org.specs2.mock.Mockito
 
-class PostServiceSpec extends Specification with PostServiceComponent with PostRepositoryComponent {
+import scala.concurrent.Future
+
+
+class PostServiceSpec extends Specification with Mockito
+    with PostsComponent with PostRepositoryComponent {
+  override val postRepository: PostRepository = mock[PostRepository]
 
   "PostService" should {
     "find post with title 'First!'" in {
+      val mockPost = mock[Future[Option[Post]]]
 
+      postRepository.findByTitle("First!") returns mockPost
+      posts.findByTitle("First!") must beEqualTo(mockPost)
     }
 
+    "get all posts" in {
+      val mockPosts = mock[Future[List[Post]]]
+
+      postRepository.getAll returns mockPosts
+      posts.getAll must equalTo(mockPosts)
+    }
   }
-≈}
+}
