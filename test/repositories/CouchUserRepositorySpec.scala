2e81b9c867d95a44304f09c2a136f7e195940c5a
James Collier <jrc.collier@gmail.com>
2014-07-03 22:38:21 +0100

    Change CouchUserRepositorySpec to use ???, so we can compile

--- test/repositories/CouchUserRepositorySpec.scala
+++ test/repositories/CouchUserRepositorySpec.scala
@@ -5,7 +5,7 @@ import org.specs2.mutable.Specification
 
 class CouchUserRepositorySpec extends Specification with Mockito
     with CouchServiceComponent with CouchUserRepositoryComponent {
-  override val couchService: CouchService = _
+  override val couchService: CouchService = ???
 
   "CouchServiceComponent" should {
     "find user joe@gmail.com" in {
