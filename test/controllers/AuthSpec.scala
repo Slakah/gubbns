b7f0a0b4d83dd5b4fbd63809b3fd9bfd8af8a9f1
James Collier <jrc.collier@gmail.com>
2014-10-04 23:39:41 +0100

    add basic integration tests for login

--- test/controllers/AuthSpec.scala
+++ test/controllers/AuthSpec.scala
@@ -1,8 +1,17 @@
 package controllers
 
-/**
- * Created by collij37 on 29/09/2014.
- */
-class AuthSpec {
+import org.specs2.mutable._
+import play.api.Logger
+import play.api.test.WithBrowser
 
+class AuthSpec extends Specification {
+  "Auth" should {
+    "allow login" in new WithBrowser {
+      browser.goTo(routes.Auth.login.url)
+      browser.fill("#email").`with`("joe.bloggs@email.com")
+      browser.fill("#password").`with`("password")
+      browser.submit("button")
+      Logger.info(browser.pageSource())
+    }
+  }
 }
