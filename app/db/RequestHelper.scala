95e2f35e3115ea13be7c656000409ba0c5392083
James Collier <jrc.collier@gmail.com>
2014-10-05 12:56:19 +0100

    revert scala to 2.10.4

--- app/db/RequestHelper.scala
+++ app/db/RequestHelper.scala
@@ -5,7 +5,7 @@ import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.ws.WSResponse
 import db.ResponseHandler.FutureResponseWithValidate
 import scala.concurrent.Future
-import scala.util.{Failure, Success}
+import scala.util.{Try, Failure, Success}
 
 object RequestHelper {
   implicit class RequestHelper(request: RequestHolder) {
@@ -29,7 +29,18 @@ object RequestHelper {
           case 200 => Success(true)
           case errorStatus => Failure(CouchException(response))
         }
-      }.flatMap(Future.fromTry(_))
+      }.flatMap(fromTry(_))
     }
+  }
+
+  /**
+   * Replace this with Future.fromTry in scala 2.11
+   * @param t
+   * @tparam A
+   * @return
+   */
+  def fromTry[A](t: Try[A]): Future[A] = t match {
+    case Success(s) => Future.successful(s)
+    case Failure(f) => Future.failed(f)
   }
 }
