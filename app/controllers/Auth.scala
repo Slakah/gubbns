ef6dc70ae474a2a4aef9ffb7f80a75a49ab3c6b7
James Collier <jrc.collier@gmail.com>
2014-10-08 00:15:44 +0100

    add unauthorised case for add blog post + fix security to allow Future[Result]

--- app/controllers/Auth.scala
+++ app/controllers/Auth.scala
@@ -28,6 +28,9 @@ trait AuthImpl extends Controller with UserRepositoryComponent {
     Ok(views.html.user.login(loginForm))
   }
 
+  def unauthorisedLogin =
+    Unauthorized(views.html.user.login(loginForm))
+
   val isoFormat = ISODateTimeFormat.dateTime
 
   def loginPost() = Action.async { implicit request =>
