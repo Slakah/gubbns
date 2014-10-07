95823952c02761147a63b148d7cfa416584771cf
James Collier <jrc.collier@gmail.com>
2014-10-07 22:48:39 +0100

    Added sketch for Blog#addPost, with spec for unauthorised

---
+++ test/controllers/BlogSpec.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import org.specs2.mock.Mockito
+import org.specs2.mutable.Specification
+import play.api.test.Helpers._
+import play.api.test.{FakeRequest, WithApplication}
+
+class BlogSpec extends Specification with Mockito {
+  "Blog" should {
+    "allow adding of posts" in {
+      "redirect when unauthorised" in new WithApplication {
+        val addPostRequest = FakeRequest(POST, "/")
+        val addPostResponse = Blog.addPost()(addPostRequest)
+        status(addPostResponse) must equalTo(UNAUTHORIZED)
+      }
+    }
+  }
+}
