f12ff7c2e0c5a43913de5f0e8bbdf6246d7e74dc
James Collier <jrc.collier@gmail.com>
2014-10-08 00:15:44 +0100

    add unauthorised case for add blog post + fix security to allow Future[Result]

--- test/controllers/BlogSpec.scala
+++ test/controllers/BlogSpec.scala
@@ -7,6 +7,7 @@ import play.api.test.{FakeRequest, WithApplication}
 
 class BlogSpec extends Specification with Mockito {
 <<<<<<< HEAD
+<<<<<<< HEAD
 
   "Blog" should {
     "allow adding of posts" in {
@@ -16,6 +17,12 @@ class BlogSpec extends Specification with Mockito {
     "allow adding of posts" in {
       "redirect when unauthorised" in new WithApplication {
 >>>>>>> Added sketch for Blog#addPost, with spec for unauthorised
+=======
+
+  "Blog" should {
+    "allow adding of posts" in {
+      "disabled when unauthorised" in new WithApplication {
+>>>>>>> add unauthorised case for add blog post + fix security to allow Future[Result]
         val addPostRequest = FakeRequest(POST, "/")
         val addPostResponse = Blog.addPost()(addPostRequest)
         status(addPostResponse) must equalTo(UNAUTHORIZED)
