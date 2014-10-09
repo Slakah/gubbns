68dbc021b94e7e3cf370ab68b682f8c07893fadd
James Collier <james.collier01@bbc.co.uk>
2014-10-09 22:56:43 +0100

    fix after rebase from master

--- test/controllers/BlogSpec.scala
+++ test/controllers/BlogSpec.scala
@@ -4,12 +4,15 @@ import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import play.api.test.Helpers._
 import play.api.test.{FakeRequest, WithApplication}
+import play.filters.csrf.CSRF
 
 class BlogSpec extends Specification with Mockito {
   "Blog" should {
     "allow adding of posts" in {
       "disabled when unauthorised" in new WithApplication {
         val addPostRequest = FakeRequest(POST, "/")
+          .withSession((CSRF.TokenName, CSRF.SignedTokenProvider.generateToken))
+
         val addPostResponse = Blog.addPost()(addPostRequest)
         status(addPostResponse) must equalTo(UNAUTHORIZED)
       }
