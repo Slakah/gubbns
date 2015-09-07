19376e7c4717b200c9c5a660ae520d83a59dbff3
James Collier <james.collier01@bbc.co.uk>
2015-09-07 23:26:30 +0100

    add couchdb sync plugin to global to work around plugin removal

--- app/Global.scala
+++ app/Global.scala
@@ -1,5 +1,15 @@
-import play.api.GlobalSettings
+import play.api.{Application, GlobalSettings}
 import play.api.mvc.WithFilters
 import play.filters.csrf.CSRFFilter
+import plugins.BlogCouchSync
 
-object Global extends WithFilters(CSRFFilter()) with GlobalSettings
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+
+object Global extends WithFilters(CSRFFilter()) with GlobalSettings {
+  override def onStart(app: Application) = {
+    Await.ready(BlogCouchSync.sync(), 5.seconds)
+    super.onStart(app)
+  }
+}
