a9bd902fc7eb9e8f13ac41a85210002566227677
James Collier <jrc.collier@gmail.com>
2016-05-07 23:33:01 +0100

    sync with couchdb before running integration tests

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,9 +1,13 @@
+import db.{PlayConfigService, WSWebService, Couch}
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
-
 import play.api.test._
 import play.api.test.Helpers._
+import plugins.BlogCouchSync
+
+import scala.concurrent.duration._
+import scala.concurrent.Await
 
 /**
  * Add your spec here.
@@ -15,11 +19,14 @@ class ApplicationSpec extends Specification {
 
   "Application" should {
 
-    "send 404 on a bad request" in new WithApplication{
+    "send 404 on a bad request" in new WithApplication {
       status(route(FakeRequest(GET, "/boum")).get) must equalTo(NOT_FOUND)
     }
 
-    "render the index page" in new WithApplication{
+    "render the index page" in new WithApplication {
+      val couch = new Couch(new WSWebService, new PlayConfigService)
+      Await.ready(new BlogCouchSync(couch).sync(), 5.seconds)
+
       val home = route(FakeRequest(GET, "/")).get
 
       status(home) must equalTo(OK)
