eb7a77c2618a0f0951fa5768623fab7fe0cff970
James Collier <jrc.collier@gmail.com>
2016-05-07 23:04:25 +0100

    dont run db sync when testing

--- app/Global.scala
+++ app/Global.scala
@@ -1,5 +1,5 @@
 import db.{PlayConfigService, WSWebService, Couch}
-import play.api.{Application, GlobalSettings}
+import play.api.{Mode, Application, GlobalSettings}
 import play.api.mvc.WithFilters
 import play.filters.csrf.CSRFFilter
 import plugins.BlogCouchSync
@@ -10,8 +10,10 @@ import scala.concurrent.duration._
 
 object Global extends WithFilters(CSRFFilter()) with GlobalSettings {
   override def onStart(app: Application) = {
-    val couch = new Couch(new WSWebService, new PlayConfigService)
-    Await.ready(new BlogCouchSync(couch).sync(), 5.seconds)
+    if (app.mode != Mode.Test) {
+      val couch = new Couch(new WSWebService, new PlayConfigService)
+      Await.ready(new BlogCouchSync(couch).sync(), 5.seconds)
+    }
     super.onStart(app)
   }
 }
